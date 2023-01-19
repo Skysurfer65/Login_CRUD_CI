f1ce4ea9584bd4b51376dd0564656131a3e6f4ba
Skysurfer65 <richard_fehling@hotmail.com>
2023-01-19 23:22:41 +0100

    classUser.test refaktored

--- tests/classUser.test.js
+++ tests/classUser.test.js
@@ -17,25 +17,27 @@ describe('Checking validateUserID() in class User', () => {
   //Variables
   let adminID;   
   
-  test('Validate "admin" User ID', () => {
+  test('Validate "admin" User ID and this.admin = true', () => {
       let user = new User("admin", null, null);
       adminID = "admin";
       expect(user.validateUserID(adminID)).toBeTruthy();
+      expect(user.admin).toBeTruthy();
     });
 
-  test('Validate newAdminID User ID', () => {
+  test('Validate newAdminID User ID and this.admin = true', () => {
         let user = new User("my1admin", null, null);
-        adminID = "my1Admin";
+        adminID = "my1admin";
         expect(user.validateUserID(adminID)).toBeTruthy();
+        expect(user.admin).toBeTruthy();
       });
 
-  test.each(goodUsers)('tested User ID (%s)', (userID) => {
+  test.each(goodUsers)('tested good User ID (%s)', (userID) => {
     let user = new User(userID, null, null);
     adminID = "admin";
     expect(user.validateUserID(adminID)).toBeTruthy(); 
   });
 
-  test.each(badUsers)('tested User ID (%s)', (userID) => {
+  test.each(badUsers)('tested bad User ID (%s)', (userID) => {
     let user = new User(userID, null, null);
     adminID = "admin";
     expect(user.validateUserID(adminID)).toBeFalsy(); 
@@ -47,12 +49,12 @@ describe('Checking validatePassword() in class User', () => {
   const goodPasswords = ["Bax1#", "2aX#", "Bax3%", "40bAx?", "20Åäö&", "LongPass##012345"];
   const badPasswords = ["", null, undefined, "P1#", "password1#", "Pass#", "Pass word1#", "TooLongPass#34567"];
 
-  test.each(goodPasswords)('tested password (%s)', (password) => {
+  test.each(goodPasswords)('tested good password (%s)', (password) => {
     let user = new User(null, password, null);
     expect(user.validatePassword()).toBeTruthy(); 
   });
   
-  test.each(badPasswords)('tested password (%s)', (password) => {
+  test.each(badPasswords)('tested bad password (%s)', (password) => {
     let user = new User(null, password, null);
     expect(user.validatePassword()).toBeFalsy(); 
   });
@@ -84,7 +86,7 @@ describe('Checking checkPassword() in class User', () => {
   //Setup test arrays
   const goodUsers = ["bax1", "Bax2", "admin", "Admin1", "Åäö20", "longUserID01234567890123456789"]; 
   const goodPasswords = ["Bax1#", "2aX#", "Bax3%", "40bAx?", "20Åäö&", "LongPass##012345"];
-  const badPasswords = [" Password1#", "Pass word1#", "TooLongPass#34567"];
+  const badPasswords = [" Password1#", "Pass word1#", "TooLongPass#34567", "Password1# "];
   const users = [];
 
   //Make a mock database
@@ -104,12 +106,14 @@ describe('Checking checkPassword() in class User', () => {
   let badPerson = new User("bax1", "Baxen1#", null);
   let badPersUsers = [badPerson];
 
-  //Do 3 bad logins with wrong pass
+  //Do 4 bad logins with wrong pass
+  let badTry = new User("bax1", null, null);
   test.each(badPasswords)('Check bad password (%s) against mockDB', (password) => {
-    let badTry = new User("bax1", password, null);
-    expect(badTry.checkPassword(badPersUsers)).toBeFalsy(); 
+    badTry.password = password;
+    expect(badTry.checkPassword(badPersUsers)).toBeFalsy();
+    expect(badPerson.nrOfAttempts).toBeLessThanOrEqual(3);
   });
-
+  //After 3 bad logins with wrong pass user deleted
   test('Empty array after 3 bad pass', () => {
     expect(badPersUsers.length).toBe(0);
   });
